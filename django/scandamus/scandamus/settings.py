8fa5c24c929d913239d6c4bdd8d685871394d2b2
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-30 20:59:45 +0900

    :sparkles:[add]login api

--- django/scandamus/scandamus/settings.py
+++ django/scandamus/scandamus/settings.py
@@ -103,6 +103,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'players.auth.CustomJWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
--- django/scandamus/scandamus/urls.py
+++ django/scandamus/scandamus/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/players/', include('players.urls')),
     path('api/game/', include('game.urls')),
 ]
