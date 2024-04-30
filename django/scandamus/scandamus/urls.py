be42763006e13340213259d66a39baeb4dbd1269
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-30 18:53:06 +0900

    :dizzy:[update]defaultRouterhはProjectRootでは呼ばない

--- django/scandamus/scandamus/urls.py
+++ django/scandamus/scandamus/urls.py
@@ -1,20 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# DefaultRouter => ViewSetに基づきRESTful APIのURLを生成
-from rest_framework.routers import DefaultRouter
-from players.views import PlayersViewSet
-# インスタンス生成
-router = DefaultRouter()
-# エンドポイント登録
-router.register(r'players', PlayersViewSet)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # ↓ router.urlsはrouter.register()で登録されたURLパターンリスト
-    path('api/', include(router.urls))
-
-    # DefaultRouterを使わずappのurlpatternsを使う場合、'app.urls'を指定
-    # path('api/app/', include('app.urls'))
     path('api/game/', include('game.urls')),
 ]
