dbff611d357a225b1ade3fadfdc7e0820825607c
hachi868 <24986912+hachi868@users.noreply.github.com>
2024-04-30 17:23:32 +0900

    :dizzy:[update]tournament period, startのデータ型に時間も持たせる

--- django/scandamus/game/models.py
+++ django/scandamus/game/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Tournament(models.Model):
     name = models.CharField(max_length=50, verbose_name="トーナメント名")
-    start = models.DateField(verbose_name="開始日")
-    period = models.DateField(verbose_name="締切")
+    start = models.DateTimeField(verbose_name="開始時間")
+    period = models.DateTimeField(verbose_name="エントリー締切")
 
     def __str__(self):
         return self.name
